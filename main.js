76315c4b58d82c5d18b02da5b6492ba29d69c8db
Blake McCarty <blake_mccarty@yahoo.com>
2022-01-25 12:06:24 -0600

    rework

--- main.js
+++ main.js
@@ -1,210 +1,205 @@
 // Access toggle switch HTML element
 
-var question = document.getElementById("questions");
-var option1 = document.getElementById("a");
-var option2 = document.getElementById("b");
-var option3 = document.getElementById("c");
-var option4 = document.getElementById("d");
-var countDown = document.getElementById("timer");
-var header = document.getElementById("header");
+const question = document.getElementById("questions");
+const option1 = document.getElementById("a");
+const option2 = document.getElementById("b");
+const option3 = document.getElementById("c");
+const option4 = document.getElementById("d");
+const buttonA = document.getElementById("butttonA");
+const buttonB = document.getElementById("butttonB");
+const buttonC = document.getElementById("butttonC");
+const buttonD = document.getElementById("butttonD");
+const countDown = document.getElementById("timer");
+
 countDown.innerHTML = 10;
 
-var richmond = document.getElementById("richmond");
-var jen = document.getElementById("jen");
-var maurice = document.getElementById("maurice");
-var roy = document.getElementById("roy");
-
-///// create an object to hold questions and answers
-const myQuestions = [
+///// create an object to hold questions and options
+const myData = [
   {
     q: "Roy has been decribed by an ex-girlfriend as...",
 
-    answers: [
-      "Emotionally Artistic",
-      "A Massive Arse",
-      "The Best Lover She Ever Had",
-      "Roy Has Never Had A Girlfriend",
+    options: [
+      "A: Emotionally Artistic",
+      "B: A Massive Arse",
+      "C: The Best Lover She Ever Had",
+      "D: Flippin Fantastic",
     ],
 
-    correct: "a",
+    correct: "buttonA",
   },
   {
     q: "What dating site does the I.T. Crowd use?",
 
-    answers: ["Halt and Catch Fire", "Lonely Hearts", "Buffle", "Crickets"],
+    options: [
+      "A: Halt and Catch Fire",
+      "B: Lonely Hearts",
+      "C: Buffle",
+      "D: Crickets",
+    ],
 
-    correct: "b",
+    correct: "buttonB",
   },
   {
     q: "How many people work in the I.T. Department",
 
-    answers: ["2", "3", "4", "5"],
+    options: ["A: 2", "B: 3", "C: 4", "D: 5"],
 
-    correct: "c",
+    correct: "buttonC",
   },
   {
     q: "Who replaces Denholm Reynholm",
 
-    answers: [
-      "Douglas Reynholm",
-      "Dennis Rhinestone",
-      "Danny Trejo",
-      "Jen Barber",
+    options: [
+      "A: Douglas Reynholm",
+      "B: Dennis Rhinestone",
+      "C: Danny Trejo",
+      "D: Jen Barber",
     ],
 
-    correct: "a",
+    correct: "buttonA",
   },
   {
     q: "Which OS does the bomb disposal robot run on",
 
-    answers: ["Windows Vista", "Windows ME", "Linux", "MenuetOS"],
+    options: ["A: Windows Vista", "B: Windows ME", "C: Linux", "D: MenuetOS"],
 
-    correct: "a",
+    correct: "buttonA",
   },
   {
-    q: "What do I like to eat in the morning?",
+    q: "What does Maurice eat for breakfast?",
 
-    answers: [
-      "Eggs, Banger, and Beans",
-      "Smartie Cereal",
-      "Wheaties",
-      "I'm More of a Brunch Man",
+    options: [
+      "A: Eggs, Banger, and Beans",
+      "B: Smarties Cereal",
+      "C: Wheaties",
+      "D: I'm More of a Brunch Man",
     ],
 
-    correct: "b",
+    correct: "buttonB",
   },
   {
     q: "What is Richmond's favorite drink",
 
-    answers: ["Blood", "Grape Juice", "Absynthe", "Red Wine"],
+    options: ["A: Blood", "B: Grape Juice", "C: Absynthe", "D: Red Wine"],
 
-    correct: "c",
+    correct: "buttonC",
   },
   {
     q: "What is the number for emergency services",
 
-    answers: ["0118 999 881 999 119 7253", "999", "911", "0909 87900222"],
+    options: [
+      "A: 0118 999 881 999 119 7253",
+      "B: 999",
+      "C: 911",
+      "D: 0909 87900222",
+    ],
 
-    correct: "a",
+    correct: "buttonA",
   },
   {
     q: "How does Denholm Reynholm meet his demise?",
 
-    answers: [
-      "Chokes On A Donut",
-      "Scalded To Death by Tea",
-      "Overwork",
-      "Jumps Out The Window",
+    options: [
+      "A: Chokes On A Biscuit",
+      "B: Scalded To Death by Tea",
+      "C: Overwork",
+      "D: Jumps Out The Window",
     ],
 
-    correct: "d",
+    correct: "buttonD",
   },
   {
     q: "Where is Reynholm Industries located?",
 
-    answers: [
-      "65 Sutton Wick Lane",
-      "9 Abbey Road",
-      "123 Carenden Road",
-      "422 Crumpet Hill",
+    options: [
+      "A: 65 Sutton Wick Lane",
+      "B: 9 Abbey Road",
+      "C: 123 Carenden Road",
+      "D: 422 Crumpet Hill",
     ],
 
-    correct: "c",
+    correct: "buttonC",
   },
 ];
 
-////store questions///
-qIndex = [];
+// // ////store questions///
+let qIndex = [];
 
-///store answers///
-optionIndex = [];
+// ///store options///
+let optionIndex = [];
 
-/// tracks current question #////
-answerIndex = [];
+// /// tracks current question #////
+let answerIndex = [];
 
 ////keep score///
-score = 0;
+var score = 0;
 
+//// game running true or false
+var gameRunning = false;
 function createArrays() {
-  for (i = 0; i < myQuestions.length; i++) {
+  for (i = 0; i < myData.length; i++) {
     ///push to single array///
-    qIndex.push(myQuestions[i].q);
+    qIndex.push(myData[i].q);
 
     ///corect answer array///
-    answerIndex.push(myQuestions[i].correct);
+    answerIndex.push(myData[i].correct);
 
-    /////  put answers in a single array////
-    optionIndex.push(myQuestions[i].answers);
+    /////  put options in a single array////
+    optionIndex.push(myData[i].options);
   }
 }
-
 function renderQuestion() {
   ////push to HTML///
   question.innerHTML = qIndex[0];
   if (qIndex[0] == undefined) {
-    question.innerHTML = "";
+    clearInterval(ref);
+    displayScore();
+    optionIndex.shift();
   }
 }
-
+console.log(optionIndex[0]);
 function renderAnswers() {
-  ///push answers from array to buttons///
+  ///push options from array to buttons///
+  if (answerIndex[0] == undefined) {
+    gameRunning = false;
 
-  option1.innerHTML = optionIndex[0][0];
-  option2.innerHTML = optionIndex[0][1];
-  option3.innerHTML = optionIndex[0][2];
-  option4.innerHTML = optionIndex[0][3];
-}
-///// make richmond pop out
-function ohDear(time) {
-  richmond.style.top = "-30px";
-  richmond.style.transition = "1s";
-  gagTimer = setInterval(function () {
-    time--;
-    console.log(time);
-    if (time === 0) {
-      clearInterval(gagTimer);
-      richmond.style.top = "200px";
-      richmond.style.transition = "1s";
-    }
-  }, 1000);
-}
-
-///this function will dim the header to allow gags to be seen better
-function dimTitle() {
-  header.style.opacity = "10%";
-  header.style.transition = "2s";
-}
-
-///when game ends bring the opacity back up
-function restoreTitle() {
-  header.style.opacity = "100%";
-  header.style.transition = "2s";
+    option1.innerHTML = "A:";
+    option2.innerHTML = "B:";
+    option3.innerHTML = "C:";
+    option4.innerHTML = "D:";
+  } else {
+    option1.innerHTML = optionIndex[0][0];
+    option2.innerHTML = optionIndex[0][1];
+    option3.innerHTML = optionIndex[0][2];
+    option4.innerHTML = optionIndex[0][3];
+    return;
+  }
 }
 
 ///this tellls me what is clicked///
 function checkAnswer(clickedId) {
-  correct = answerIndex[0];
-  if (clickedId === correct) {
-    //// if ID matched current correct////
-    score += 10;
-    nextQuestion();
-  } else ohDear(2);
-  nextQuestion();
+  if (answerIndex[0] !== undefined) {
+    if (clickedId === answerIndex[0]) {
+      //// if ID matched current correct////
+      score += 10;
+      nextQuestion();
+    } else nextQuestion();
+  } else return;
 }
-
 //// force next question after 10secs
 var ref;
 function timer(count) {
   ref = setInterval(function () {
     count--;
     countDown.innerHTML = count;
-
-    if (count === 0) {
+    if (gameRunning) {
+      if (count === 0) {
+        clearInterval(ref);
+        nextQuestion();
+      }
+    } else {
       clearInterval(ref);
-      nextQuestion();
-    } else if (qIndex[0] == undefined) {
-      clearInterval(ref);
-      displayScore();
+      countDown.innerHTML = 0;
     }
   }, 1000);
 }
@@ -222,14 +217,15 @@ function nextQuestion() {
 
 function displayScore() {
   question.innerHTML = "Congratulations you scored " + score + "%";
-  restoreTitle();
 }
 
 ////start game bttn function ///
 function startGame() {
-  createArrays();
-  renderQuestion();
-  renderAnswers();
-  timer(10);
-  dimTitle();
+  if (qIndex.length < 1) {
+    gameRunning = true;
+    createArrays();
+    renderQuestion();
+    renderAnswers();
+    timer(10);
+  } else return;
 }
